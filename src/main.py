ae36cdad16b6e2412fd488704872252ad9618abe
Radoslav Filipov <fedarradi@gmail.com>
2025-10-03 15:56:07 +0300

    remove filesaving

--- src/main.py
+++ src/main.py
@@ -11,10 +11,6 @@ def beautify_html(data : str) -> str:
     soup = BeautifulSoup(data, "html.parser")
     return soup.prettify()
 
-def write_in_data_in_file(data, file_name : str):
-    with open(file_name, "w", encoding="utf-8") as f:
-        f.write(data)
-
 def find_by_class(html_data : str, class_name : str, tag=None) -> str:
     soup = BeautifulSoup(html_data, "html.parser")
 
@@ -22,24 +18,17 @@ def find_by_class(html_data : str, class_name : str, tag=None) -> str:
         return soup.find_all(tag, class_=class_name)
     
     return soup.find_all(class_=class_name)
-
-def get_data_from_file(file_name : str):
-    with open(file_name, "r", encoding="utf-8") as f:
-        data = f.read()
-        return data
     
-def save_main_page_html(url : str, file_name : str):
+def get_main_page_html(url : str):
     raw_data = get_web_page_content(url)
     data = beautify_html(raw_data)
-    write_in_data_in_file(data, file_name)
-
+    return data
 
 def last_page_number(s: str):
     matches = re.findall(r'\bPage\s+(\d+)', s)
     return int(matches[-1]) if matches else None
 
-def extract_page_count(file_name : str) -> int:
-    html = get_data_from_file(file_name)
+def extract_page_count(html : str) -> int:
     elements = find_by_class(html, "pagination-list")
 
     if not elements:
@@ -63,13 +52,10 @@ def get_all_pages_urls(url, page_count):
 
 def main():
     url = "https://www.olx.bg/ads/q-%D0%B2%D0%BE%D0%BB%D0%B0%D0%BD/"
-    file_name_mane_page = "beutiful_html.html"
 
-
-    save_main_page_html(url, file_name_mane_page)
-    page_count = extract_page_count(file_name_mane_page)
-    urls = get_all_pages_urls(url, page_count)
-   
-
+    html_data = get_main_page_html(url)
+    page_count = extract_page_count(html_data)
+    urls = get_all_pages_urls(url, page_count)   
+    print(urls)
 
 main()
