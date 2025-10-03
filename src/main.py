bcab0989da54f515bcdfe1e80743aedf1f428d82
Radoslav Filipov <fedarradi@gmail.com>
2025-10-03 15:50:11 +0300

    find all urls for all pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def get_web_page_content(url, timeout=10):
     resp = requests.get(url, timeout=timeout)
@@ -22,27 +23,53 @@ def find_by_class(html_data : str, class_name : str, tag=None) -> str:
     
     return soup.find_all(class_=class_name)
 
-def get_data_from_file(file_name):
+def get_data_from_file(file_name : str):
     with open(file_name, "r", encoding="utf-8") as f:
         data = f.read()
         return data
     
-def save_main_page_html(url, file_name):
+def save_main_page_html(url : str, file_name : str):
     raw_data = get_web_page_content(url)
     data = beautify_html(raw_data)
     write_in_data_in_file(data, file_name)
 
-def extract_other_pages_links(file_name, class_name):
-    data_old = get_data_from_file(file_name)
-    print(find_by_class(data_old, class_name))
+
+def last_page_number(s: str):
+    matches = re.findall(r'\bPage\s+(\d+)', s)
+    return int(matches[-1]) if matches else None
+
+def extract_page_count(file_name : str) -> int:
+    html = get_data_from_file(file_name)
+    elements = find_by_class(html, "pagination-list")
+
+    if not elements:
+        print(f"No elements with class pagination-list found.")
+        return
+    
+    combined_html = ""
+    for el in elements:
+        combined_html += str(el) + "\n"
+
+    return last_page_number(combined_html)
+
+
+def get_all_pages_urls(url, page_count):
+    page_urls = []
+    for i in range(1, page_count + 1):
+        new_url = url + "?page=" + str(i)
+        page_urls.append(new_url)
+
+    return page_urls
 
 def main():
     url = "https://www.olx.bg/ads/q-%D0%B2%D0%BE%D0%BB%D0%B0%D0%BD/"
-    file_name = "beutiful_html.html"
-    save_main_page_html(url, file_name)
+    file_name_mane_page = "beutiful_html.html"
 
-    class_name = "pagination-list"
-    extract_other_pages_links(file_name, class_name)
+
+    save_main_page_html(url, file_name_mane_page)
+    page_count = extract_page_count(file_name_mane_page)
+    urls = get_all_pages_urls(url, page_count)
+   
 
 
 main()
